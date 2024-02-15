a7b312c5c588c787e0515d3a965373cf6e7902a1
Imran Chowdhury <144194950+imranncc@users.noreply.github.com>
2024-02-15 14:23:21 +0530

    Add files via upload

---
+++ src/FindMinimum.java
@@ -0,0 +1,29 @@
+/* Assignment 4 (100 marks in total; 5% of the final score of this course)
+ *
+ * Question 1 (20 marks)
+ *
+ * Write a Java program, use 'subroutine' to finds the minimum value of
+ * the first N elements of an array of type int.
+ * Meanwhile, your code can throw an IllegalException (custom run-time
+ * exception, which can be one provided by Java) if N is not in the range
+ * of the length of input array.
+ *
+ * Input: user input array and number N.
+ * Output: the minimum value of the first N elements of the array.
+ *
+ * Note: The array and N are parameters to the subroutine.
+ * Users will input an array with no specific length.
+ *
+ */
+
+import java.util.Scanner;
+
+public class Q1_FindMinimum {
+    /* place your subroutine code here */
+
+    public static void main(String[] args) {
+        /* place your code to run your subroutine here */
+    }
+}
+
+
---
+++ src/LambdaExpression.java
@@ -0,0 +1,25 @@
+/* Assignment 4 (100 marks in total; 5% of the final score of this course)
+ *
+ * Question 2 (20 marks)
+ *
+ * Use a "subroutine" and a lambda expression to implement function
+ * f(x,y) = sqrt(x*x-y*y), both x and y are real numbers.
+ * Meanwhile, your code can throw an IllegalException (custom run-time exception,
+ * which can be one provided by Java) if the value of (x*x-y*y) is negative.
+ *
+ * INPUT: the value of x and y (user input)
+ * OUTPUT: the value of sqrt(x*x-y*y)
+ *
+ * Note: output must be displayed using four significant digits after decimal point.
+ *
+ */
+
+import java.util.Scanner;
+
+public class Q2_LambdaExpression {
+    /* place your subroutine code here */
+
+    public static void main(String[] args){
+        /* place your code to run your subroutine here */
+    }
+}
